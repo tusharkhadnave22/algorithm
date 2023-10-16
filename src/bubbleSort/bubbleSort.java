32f55aa427e9895537312fa1edace2f8a2dcca78
Tushar K <tushar.khandave22@gmail.com>
2023-10-16 13:01:55 +0530

    second commit

---
+++ src/bubbleSort/bubbleSort.java
@@ -0,0 +1,27 @@
+package bubbleSort;
+
+public class bubbleSort {
+    public static int[] bubble(int [] nums) {
+    	int n=nums.length;
+    	for(int i=n-2;i>=0;i--) {
+    		for(int j=0;j<=i;j++) {
+    			if(nums[j]>nums[j+1]) {
+    				int temp=nums[j];
+    				nums[j]=nums[j+1];
+    				nums[j+1]=temp;
+    			}
+    		}
+    	}
+    	return nums;
+    }
+	public static void main(String[] args) {
+		
+        int []a=new int[] {5,4,3,2,1};
+        int []ans=bubble(a);
+        for(int x:ans) {
+        	System.out.print(x);
+        }
+	
+
+}
+}
